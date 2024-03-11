f8a6248f4339bb01a099891e5d493c91af9999e7
bujyu <bujyu.uo@gmail.com>
2024-03-11 22:05:37 +0800

    Update some implementations
    - (task.py) Support to modify the value of all interpolation types
    - (task.py) Extend the function of task file
    - (task.py) fix some wrong implementation of task
    - (cli.py)  Refine the implementation of cli
    - Update documentation of sample task file

--- cli.py
+++ cli.py
@@ -6,16 +6,17 @@ import sys
 from package import mot
 from package import task
 
-def check_magic(file, magic):
-    with open(file, "rb") as f:
-        data = f.read(len(magic))
+def _check_magic(file: str, magic: str):
+    with open(file, "rb") as fobj:
+        data = fobj.read(len(magic))
     if data == None:
         return False
     elif data.decode("ASCII") != magic:
         return False
     return True
 
-def _dump_json_to_file(file, mobj: mot):    
+
+def _dump_json_to_file(file: str, mobj: mot):    
     import jsonpickle # pip install jsonpickle
     import json
 
@@ -24,55 +25,74 @@ def _dump_json_to_file(file, mobj: mot):
         json.dump(json.loads(serialized), fp=f, indent=2)
 
 
-def dump_mot_as_json(args):
-    if args.output is None:
-        args.output = args.file + ".json"
+def dump_mot_as_json(args: argparse, files: list[pathlib.Path], basepath: pathlib.Path):
+    for file in files:
+        if basepath is not None:
+            ofilepath = basepath / f"{file.name}.json"
+        else:
+            ofilepath = file.parent / f"{file.name}.json"
 
-    mobj = mot.MotFile()
-    with open(args.file, "rb") as f:
-        mobj.fromFile(f)
+        mobj = mot.MotFile()
+        with open(str(file), "rb") as fobj:
+            mobj.fromFile(fobj)
 
-    _dump_json_to_file(args.output, mobj)
+        print(f"+ {file.name}: ")
+
+        _dump_json_to_file(str(ofilepath), mobj)
 
 
-def apply_and_export(args):
-    import pathlib
-
+def apply_and_export(args: argparse, files: list[pathlib.Path], basepath: pathlib.Path):
     if args.task is None:
-        print("No task specified, only export ...", file=sys.stderr)
-    if args.output is None:
-        pth = pathlib.Path(args.file)
-        args.output = str(pth.parent) + "\\" + str(pth.name) + "_mod" + str(pth.suffix)
-    elif pathlib.Path(args.output).suffix == ".bxm":
-        if pathlib.Path(args.output).samefile(args.file):
-            raise UserWarning("file and output have target to same file ...")
+        raise UserWarning("No task specified ...")
+
+    for file in files:
+        if basepath is not None:
+            ofilepath = basepath / f"mod_{file.name}"
+        else:
+            ofilepath = file.parent / f"mod_{file.name}"
     
-    mobj = mot.MotFile()
-    with open(args.file, "rb") as f:
-        mobj.fromFile(f)
+        mobj = mot.MotFile()
+        with open(str(file), "rb") as fobj:
+            mobj.fromFile(fobj)
 
-    for it in enumerate(mobj.records):
-        task.apply(args.task, it)
+        print(f"+ {file.name}: ")
 
-    _dump_json_to_file(args.output + ".json", mobj)
+        if args.debug:
+            if basepath is not None:
+                debug_json_filepath = basepath / f"{file.name}.json"
+            else:
+                debug_json_filepath = file.parent / f"{file.name}.json"
+            _dump_json_to_file(str(debug_json_filepath), mobj)
 
-    with open(args.output, "wb") as f:
-        mobj.writeToFile(f)
+        ret = False
+        for it in enumerate(mobj.records):
+            ret = ret | task.apply(args.task, it)
+
+        # if no modification applied, do not write out mot object
+        if not ret:
+            continue
+        
+        with open(ofilepath, "wb") as fobj:
+            mobj.writeToFile(fobj)
+
+        if args.debug:
+            debug_json_filepath = ofilepath.parent / f"{ofilepath.name}.json"
+            _dump_json_to_file(str(debug_json_filepath), mobj)
 
 
-def match(args):
-    import pathlib
-
+def match(args: argparse, files: list[pathlib.Path], output_path: pathlib.Path):
     if args.task is None:
         raise UserWarning("No task specified ...")
     
-    mobj = mot.MotFile()
-    with open(args.file, "rb") as f:
-        mobj.fromFile(f)
+    for file in files:
+        mobj = mot.MotFile()
+        with open(str(file), "rb") as fobj:
+            mobj.fromFile(fobj)
 
-    print(f"{args.file}:")
-    for it in enumerate(mobj.records):
-        task.match(args.task, it)
+        print(f"+ {file.name}: ")
+        ret = False
+        for it in enumerate(mobj.records):
+            ret = ret | task.match(args.task, it)
 
 
 action_table = {
@@ -82,28 +102,52 @@ action_table = {
 }
 
 
-def main(args):
+def main(args: argparse, files: list[pathlib.Path], output_path: pathlib.Path):
     if args.action in action_table:
-        action_table[args.action](args)
+        action_table[args.action](args, files, output_path)
         return
     raise UserWarning("Not supported action ...")
         
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file", "-f", help="file .mot", type=str, required=True)
     parser.add_argument("--action", "-a", help="specified the action for cli", type=str, choices=[k for k in action_table], required=True)
-    parser.add_argument("--output", "-o", help="output file path", type=str)
-    # For action "apply_and_export" / "match"
+    parser.add_argument("--output", "-o", help="output directory", type=str)
     parser.add_argument("--task", "-t", help="Task file for modifying the mot file", type=str)
+    parser.add_argument("--debug", "-d", help="Generate debug information", action="store_true")
+    parser.add_argument('files', help="file .mot or directory includes .mot", nargs='+')
     args = parser.parse_args()
 
-    # check unify argument "file" here
-    if not os.path.exists(args.file):
+    # check ambiguous arguments here
+    file_list = []
+    for arg in args.files:
+        pth = pathlib.Path(arg)
+        if pth.is_file():
+            if pth.suffix != ".mot":
+                continue
+            file_list.append(pth)
+        elif pth.is_dir():
+            print("> Expand the directory (no nested expand) ...")
+            file_list.extend([child for child in pth.glob("*.mot") if child.is_file()])
+        else:
+            raise UserWarning(f"Unsupported file type ... {pth.absolute()}")
+    '''
+    for file in file_list:
+        if not _check_magic(str(file), "mot"):
+            raise UserWarning(f"Not supported mot file ... {file.absolute()}")
+    '''
+    if len(file_list) == 0:
         parser.print_help()
-        raise UserWarning("File is not existing: {} ...".format(args.file))
-    elif not check_magic(args.file, "mot"):
-        # "mot" at the head 3 bytes
-        raise UserWarning("Not supported mot file ...")
+        raise UserWarning(f"No valid file ...")
 
-    main(args)
+    # check output here
+    basepath = None
+    if args.output is not None:
+        basepath = pathlib.Path(args.output)
+        if not basepath.exists():
+            print("> Create output directory ...")
+            basepath.mkdir(parents=True)
+        elif not basepath.is_dir():
+            raise UserWarning("Argument \"output\" does not target directory")
+
+    main(args, file_list, basepath)
--- package/task.py
+++ package/task.py
@@ -5,10 +5,14 @@ from . import mot
 
 _task_cond_op = {
     "==": lambda a, b: a == b,
+    "!=": lambda a, b: a != b,
     ">=": lambda a, b: a >= b,
     "<=": lambda a, b: a <= b,
     ">":  lambda a, b: a > b,
-    "<":  lambda a, b: a < b
+    "<":  lambda a, b: a < b,
+    "&":  lambda a, b: bool(a & b),
+    "|":  lambda a, b: bool(a | b),
+    "BMSK": lambda a, b: (a & b) == b 
 }
 
 _record_modifier_op = {
@@ -21,21 +25,78 @@ _record_modifier_op = {
 }
 
 def _TypeConst(op: Callable, rec: mot.MotRecord, rhs):
-    op(rec.interpolation.value, rhs)
-    op(rec.value, rhs)
+    rec.interpolation.value = op(rec.interpolation.value, rhs)
+    rec.value = op(rec.value, rhs)
 
 def _TypeValues(op: Callable, rec: mot.MotRecord, rhs):
     new_values = [op(val, rhs) for val in rec.interpolation.values]
     rec.interpolation.values = new_values
 
+def _TypeInterpol2(op: Callable, rec: mot.MotRecord, rhs):
+    rec.interpolation.p = op(rec.interpolation.p, rhs)
+    new_values = [op(val, rhs) for val in rec.interpolation.values]
+    rec.interpolation.values = new_values
+
+def _TypeInterpol3(op: Callable, rec: mot.MotRecord, rhs):
+    rec.interpolation.p = op(rec.interpolation.p, rhs)
+    new_values = [op(val, rhs) for val in rec.interpolation.values]
+    rec.interpolation.values = new_values
+
+def _TypeInterpolSpline(op: Callable, rec: mot.MotRecord, rhs):
+    for spline in rec.interpolation.splines:
+        spline.value = op(spline.value, rhs)
+
+def _TypeInterpol5(op: Callable, rec: mot.MotRecord, rhs):
+    rec.interpolation.p = op(rec.interpolation.p, rhs)
+    for spline in rec.interpolation.splines:
+        spline.value = op(spline.value, rhs)
+
+def _TypeInterpol6(op: Callable, rec: mot.MotRecord, rhs):
+    rec.interpolation.p = op(rec.interpolation.p, rhs)
+    for spline in rec.interpolation.splines:
+        spline.value = op(spline.value, rhs)
+
+def _TypeInterpol7(op: Callable, rec: mot.MotRecord, rhs):
+    rec.interpolation.p = op(rec.interpolation.p, rhs)
+    for spline in rec.interpolation.splines:
+        spline.value = op(spline.value, rhs)
+
+def _TypeInterpol8(op: Callable, rec: mot.MotRecord, rhs):
+    rec.interpolation.p = op(rec.interpolation.p, rhs)
+    for spline in rec.interpolation.splines:
+        spline.value = op(spline.value, rhs)
+
 _record_modifier = {
     # limited support interpolation type
     0: _TypeConst,
-    1: _TypeValues
+    1: _TypeValues,
+    2: _TypeInterpol2,
+    3: _TypeInterpol3,
+    4: _TypeInterpolSpline,
+    5: _TypeInterpol5,
+    6: _TypeInterpol6,
+    7: _TypeInterpol7,
+    8: _TypeInterpol8
 }
+
+def _util_conv_int(v: int|str) -> int:
+    if type(v) == int:
+        return v
+
+    _numeric_map = {
+        'b': lambda v: int(v, 2),
+        'x': lambda v: int(v, 16)
+    }
+    if v[0] == '0': # special numeric form
+        if v[1] in _numeric_map:
+            return _numeric_map[v[1]](v)
+        return int(v, 8)
+    return int(v) # guess is decimal
+
 
 def _task_op_conditon(t, it: tuple[int, mot.MotRecord], callback: Callable = None) -> bool:
     cond_expected = True
+    condition_strings = []
     for cond in t['conditions']:
         if cond['operator'] not in _task_cond_op:
             raise UserWarning(f"Unsupported condition operator: {cond['operator']}")
@@ -44,15 +105,23 @@ def _task_op_conditon(t, it: tuple[int, mot.MotRecord], callback: Callable = Non
 
         cond_op = _task_cond_op[cond['operator']]
         a = it[1].__dict__[cond['field']]
-        b = cond['value']
+        b = _util_conv_int(cond['value'])
         if cond_expected != cond_op(a, b):
             return False
+        
+        if callback != None:
+            _field_repr = {
+                "_default": lambda v: f"{v}",
+                "boneIndex": lambda v: f"{v} ({v:0x})"
+            }
+            field_repr = _field_repr['_default']
+            if cond['field'] in _field_repr:
+                field_repr = _field_repr[cond['field']]
+            astr = field_repr(a)
+            condition_strings.append(f"\tfield:[{cond['field']}: {astr}] operator:[{cond['operator']}] value:[{b}]")
     
     # call callable when full matching only
     if callback != None:
-        condition_strings = []
-        for cond in t['conditions']:
-            condition_strings.append(f"\tfield:[{cond['field']}] operator:[{cond['operator']}] value:[{cond['value']}]")
         callback("\n".join(condition_strings))
 
     return True
@@ -71,7 +140,7 @@ def _task_op_modifier(t, it: tuple[int, mot.MotRecord]) -> None:
         modifier(modifier_op, it[1], m['value'])
 
 
-def apply(path: str, it: tuple[int, mot.MotRecord]):
+def apply(path: str, it: tuple[int, mot.MotRecord]) -> bool:
     import json
 
     jobj = None
@@ -80,14 +149,17 @@ def apply(path: str, it: tuple[int, mot.MotRecord]):
     if jobj is None:
         raise RuntimeError("Task file")
 
+    ret = False
     for t in jobj:
         # check conditions
         if not _task_op_conditon(t, it):
             continue
-        print(f"Record[{it[0]}] matches condition, do task modifier ...")
+        print(f"Record[{it[0]}] matches condition, do task modifier ...", file=sys.stderr)
+        ret = True
 
         # apply modifications
         _task_op_modifier(t, it)
+    return ret
 
 
 def match(path: str, it: tuple[int, mot.MotRecord]):
@@ -104,5 +176,5 @@ def match(path: str, it: tuple[int, mot.MotRecord]):
         _task_op_conditon(
             t, 
             it, 
-            callback=lambda cond_str: print(f"Record[{it[0]}] matches conditions ...\n{cond_str}")
+            callback=lambda cond_str: print(f"Record[{it[0]}] matches conditions ...\n{cond_str}", file=sys.stderr)
         )
